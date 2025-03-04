bab40934f4260ff4f6724cb0da8a5ca49427921d
JesusFlores0199 <a20204669@idat.pe>
2025-03-03 21:29:21 -0500

    initial commit

---
+++ src/main/java/edu/pe/idat/demo_idat/DemoIdatApplication.java
@@ -0,0 +1,13 @@
+package edu.pe.idat.demo_idat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoIdatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoIdatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/pe/idat/demo_idat/DemoIdatApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.pe.idat.demo_idat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoIdatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
